182b32b7dbdb121c6b07017426ac982d0692da4a
skyvow <724355924@qq.com>
2024-04-28 21:06:44 +0800

    feat: update templates for cli

--- packages/miniprogram.cli/templates/miniprogram-component/__tests__/index.spec.ts
+++ packages/miniprogram.cli/templates/miniprogram-component/__tests__/index.spec.ts
@@ -22,8 +22,21 @@ function getId () {
 
 describe('DemoComponent', () => {
   beforeAll(() => {
-    id = simulate.load(path.resolve(__dirname, '../src/index'), 'dora-demo-component')
+    id = simulate.load(path.resolve(__dirname, '../src/index'), 'dora-demo-component', { less: true })
   })
 
   mountTest(getId)
+
+  test('mount correctly', () => {
+    const wrapper = simulate.render(id)
+    wrapper.attach(document.createElement('parent-wrapper'))
+    expect(wrapper.querySelectorAll('.dora-demo-component').length).toBe(1)
+    expect(wrapper.toJSON()).toMatchSnapshot()
+  })
+
+  test('should support to change className', () => {
+    const wrapper = simulate.render(id, { prefixCls: 'dora-demo-component-custom' })
+    wrapper.attach(document.createElement('parent-wrapper'))
+    expect(wrapper.querySelectorAll('.dora-demo-component-custom').length).toBe(1)
+  })
 })
